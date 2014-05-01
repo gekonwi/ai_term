7ded8fbfea7dc72c10e19e9bb8b79941fdde7631
Georg Konwisser <gekonwi@gmail.com>
2014-05-01 00:46:49 -0400

    current state

--- experiment_1/script.R
+++ experiment_1/script.R
@@ -1,5 +1,3 @@
-setwd("..")
-
 library("neuralnet")
 
 stores <- read.csv("data/stores.csv", header=TRUE)
@@ -7,6 +5,18 @@ features <- read.csv("data/features.csv", header=TRUE)
 train <- read.csv("data/train.csv", header=TRUE)
 merged <- merge(stores, features)
 merged <- merge(merged, train)
+
+# add an id column as a combination of store and department
+store_dep <- paste(merged[, 'Store'], merged[, 'Dept'], sep = "-")
+merged <- cbind(store_dep, merged)
+
+# turn the qualitative columns into binary quantitative columns
+# e.g. Type: {A, B, A, B, B}
+# becomes: TypeA: {1, 0, 1, 0, 0}, TypeB: {0, 1, 0, 1, 1}
+bin_store_dep <- model.matrix(~-1 + store_dep, data = merged)
+bin_date <- model.matrix(~-1 + Date, data = merged)
+bin_is_holiday <- model.matrix(~-1 + IsHoliday, data = merged)
+bin_type <- model.matrix(~-1 + Type, data = merged)
 
 
 net <- neuralnet(Weekly_Sales~Store+Date+isHoliday,  train_data,  hidden = 17, threshold = 0.005,
